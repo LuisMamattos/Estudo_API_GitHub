1a4b2cf5ac3114c95fc0b909495c0d089ab266cd
Luis Miguel de Avila Mattos <luismattos@weg.net>
2025-08-27 17:04:13 -0300

    feat: 'favoritos' adicionado

---
+++ app/favoritosComp.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Star as StarIcon, AtSignIcon } from "lucide-react";
+import fundodiv from "@/images/fundo3.avif";
+import fundofavoritos from "@/images/fundoStar.jpg";
+import { User, Repo } from "@/app/types";
+
+interface FavoritosProps {
+  // Usuários favoritos
+  favoriteUsers: User[];
+  toggleFavorite: (user: User) => void;
+
+  // Repositórios favoritos
+  favoriteRepos: Repo[];
+  toggleFavoriteR: (repo: Repo) => void;
+}
+export default function Favoritos({
+  favoriteRepos,
+  toggleFavoriteR,
+  favoriteUsers,
+  toggleFavorite,
+}: FavoritosProps) {
+  return (
+    <div className="flex flex-col gap-3 p-3">
+      {/* Perfis favoritos */}
+      <Card
+        className="bg-white rounded-xl w-[235px] p-3"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(${fundodiv.src})`,
+        }}
+      >
+        <CardHeader>
+          <CardTitle className="flex items-center text-center text-lg font-bold">
+            <StarIcon className="size-5 mr-2 fill-amber-300 stroke-amber-500" />
+            Perfis
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent className="rounded-xl p-0 w-[222px] h-[200px] overflow-y-auto flex flex-col gap-2">
+          {favoriteUsers.map((user) => (
+            <Card
+              key={user.login}
+              className="flex p-1 hover:bg-muted cursor-pointer"
+              style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(${fundofavoritos.src})`,
+              }}
+            >
+              <CardContent
+                onClick={() =>
+                  (window.location.href = `/github/user/${user.login}`)
+                }
+                className="flex items-center gap-2 font-semibold truncate"
+              >
+                <Avatar className="w-8 h-8 flex-shrink-0 border-1 border-blue-700">
+                  <AvatarImage src={user.avatar_url} alt={user.login} />
+                </Avatar>
+                <AtSignIcon className="size-4 flex-shrink-0" />
+                {user.login}
+              </CardContent>
+            </Card>
+          ))}
+        </CardContent>
+      </Card>
+
+      {/* Repositórios favoritos */}
+      <Card
+        className="bg-white rounded-xl w-[235px] p-3"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(${fundodiv.src})`,
+        }}
+      >
+        <CardHeader>
+          <CardTitle className="flex items-center text-center text-lg font-bold">
+            <StarIcon className="size-5 mr-2 fill-amber-300 stroke-amber-500" />
+            Repositórios
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent className="rounded-xl p-0 w-[222px] h-[200px] overflow-y-auto flex flex-col gap-2">
+          {favoriteRepos?.map((repo) => (
+            <Card
+              onClick={() => window.open(repo.html_url, "_blank")}
+              key={repo.id}
+              className="flex p-1 hover:bg-muted cursor-pointer"
+              style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(${fundofavoritos.src})`,
+              }}
+            >
+              <CardContent className="flex flex-col gap-1 w-full">
+                {/* Linha de nome */}
+                <div className="flex justify-between items-center">
+                  <div className="truncate font-bold">{repo.name}</div>
+                  <div>
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        toggleFavoriteR && toggleFavoriteR(repo);
+                      }}
+                      className="p-0 z-50"
+                    >
+                      <StarIcon className="w-4 h-4 flex-shrink-0 fill-yellow-400 text-blue-700" />
+                    </button>
+                  </div>
+                </div>
+
+                {/* Dono e linguagem */}
+                <div className="flex justify-between items-center text-xs">
+                  <span className=" font-semibold">@{repo.owner?.login}</span>
+                  {repo.language && (
+                    <span className="text-blue-800 font-semibold italic">
+                      {repo.language}
+                    </span>
+                  )}
+                </div>
+
+                {/* Estrelas */}
+                <div className="flex text-center items-center text-xs text-muted-foreground">
+                  <StarIcon className="w-4 h-4" /> {repo.stargazers_count}
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-
 import { useForm } from "react-hook-form";
 import {
   Form,
@@ -14,25 +13,30 @@ import {
   FormMessage,
   FormField,
 } from "@/components/ui/form";
-import { AtSignIcon, Search } from "lucide-react";
+import { AtSignIcon, Search, Star } from "lucide-react";
 import fundo from "@/images/fundo2.jpg";
 import fundodiv from "@/images/fundo3.avif";
+import Favoritos from "./favoritosComp";
+import { User, Repo } from "@/app/types";
 
-interface User {
-  login: string;
-  name: string | null;
-  avatar_url: string;
-  bio: string | null;
-  email: string | null;
-  location: string | null;
-  html_url: string;
-}
 export default function HomePage() {
   const [users, setUsers] = useState<User[]>([]);
+  const [favoriteUsers, setFavoriteUsers] = useState<User[]>(() => {
+    if (typeof window !== "undefined") {
+      const stored = localStorage.getItem("favoriteUsers");
+      return stored ? JSON.parse(stored) : [];
+    }
+    return [];
+  });
 
   const form = useForm<{ username: string }>({
     defaultValues: { username: "" },
   });
+
+  // Salvar favoritos no localStorage
+  useEffect(() => {
+    localStorage.setItem("favoriteUsers", JSON.stringify(favoriteUsers));
+  }, [favoriteUsers]);
 
   async function handleSearch(data: { username: string }) {
     if (!data.username) return;
@@ -44,6 +48,38 @@ export default function HomePage() {
     setUsers(result.items || []);
   }
 
+  function toggleFavorite(user: User) {
+    const isFav = favoriteUsers.some((fav) => fav.login === user.login);
+    if (isFav) {
+      setFavoriteUsers(favoriteUsers.filter((fav) => fav.login !== user.login));
+    } else {
+      setFavoriteUsers([...favoriteUsers, user]);
+    }
+  }
+  // Estado para os repositórios favoritos
+  const [favoriteRepos, setFavoriteRepos] = useState<Repo[]>(() => {
+    if (typeof window !== "undefined") {
+      const stored = localStorage.getItem("favoriteRepos");
+      return stored ? JSON.parse(stored) : [];
+    }
+    return [];
+  });
+
+  // Salvar favoritos de repositórios no localStorage
+  useEffect(() => {
+    localStorage.setItem("favoriteRepos", JSON.stringify(favoriteRepos));
+  }, [favoriteRepos]);
+
+  // Alternar favorito de repositório
+  function toggleFavoriteR(repo: Repo) {
+    const isFav = favoriteRepos.some((fav) => fav.id === repo.id);
+    if (isFav) {
+      setFavoriteRepos(favoriteRepos.filter((fav) => fav.id !== repo.id));
+    } else {
+      setFavoriteRepos([...favoriteRepos, repo]);
+    }
+  }
+
   return (
     <div
       className="fixed top-0 left-0 flex flex-col w-full h-full"
@@ -53,6 +89,7 @@ export default function HomePage() {
         backgroundImage: `url(${fundo.src})`,
       }}
     >
+      {/* Barra superior */}
       <div className="z-50 rounded-b-lg flex justify-between items-center bg-blue-950 p-4 w-full">
         <h1 className="text-2xl font-bold text-amber-50 truncate">
           Buscar Usuários do GitHub
@@ -79,7 +116,6 @@ export default function HomePage() {
                   </FormItem>
                 )}
               />
-
               <Button className="bg-blue-950" type="submit">
                 <Search />
               </Button>
@@ -87,38 +123,65 @@ export default function HomePage() {
           </Form>
         </div>
       </div>
-      <div className="pt-16 flex flex-col space-y-6 items-center text-center">
-        <div className="grid gap-4 w-[700]">
+
+      {/* Resultados da busca */}
+      <div className="pt-16 flex space-y-6 justify-between items-center text-center">
+        <div className="grid gap-4 w-[700px] p-3">
           {users.slice(0, 5).map((user) => (
             <Card
               key={user.login}
-              className="cursor-pointer hover:bg-muted transition "
-              onClick={() =>
-                (window.location.href = `/github/user/${user.login}`)
-              }
+              className="cursor-pointer hover:bg-muted transition"
               style={{
                 backgroundSize: "cover",
                 backgroundImage: `url(${fundodiv.src})`,
               }}
             >
-              <CardHeader className="flex flex-row items-center gap-4">
-                <Avatar className="size-15">
-                  <AvatarImage src={user.avatar_url} alt={user.login} />
-                </Avatar>
-                <span className="font-medium text-2xl">
-                  <p className="flex items-center gap-1">
-                    <AtSignIcon className="size-3" />
-                    {user.login}
-                  </p>
-                </span>
+              <CardHeader className="flex flex-row items-center justify-between gap-4">
+                <div
+                  onClick={() =>
+                    (window.location.href = `/github/user/${user.login}`)
+                  }
+                  className="flex flex-row items-center gap-4 cursor-pointer truncate"
+                >
+                  <Avatar className="size-15">
+                    <AvatarImage src={user.avatar_url} alt={user.login} />
+                  </Avatar>
+                  <span className="font-medium text-2xl">
+                    <p className="flex items-center gap-1">
+                      <AtSignIcon className="size-6" />
+                      {user.login}
+                    </p>
+                  </span>
+                </div>
+                <Button
+                  variant={"ghost"}
+                  onClick={() => toggleFavorite(user)}
+                  className="p-0 bg-transparent hover:bg-transparent hover:text-yellow-500"
+                >
+                  <Star
+                    className={`w-5 h-5 ${
+                      favoriteUsers.some((fav) => fav.login === user.login)
+                        ? "fill-yellow-400 text-yellow-400"
+                        : "text-gray-400"
+                    }`}
+                  />
+                </Button>
               </CardHeader>
             </Card>
           ))}
         </div>
+
         <img
           src="https://raw.githubusercontent.com/aayushgoyal/aayushgoyal/master/github.gif"
           alt="github dançarino"
-          className="w-100 h-100 rounded-full"
+          className="rounded-full w-32 h-32 md:w-32 md:h-32 lg:w-100 lg:h-100"
+        />
+
+        <Favoritos
+          favoriteUsers={favoriteUsers}
+          toggleFavorite={toggleFavorite}
+          favoriteRepos={favoriteRepos}
+          toggleFavoriteR={toggleFavoriteR}
         />
       </div>
     </div>
